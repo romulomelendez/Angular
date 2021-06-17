0d3e4720d4a0a0304e5e1cc2810c62e0e6b61242
romulomelendez <romulomelendez.97@gmail.com>
2021-06-17 11:08:38 -0300

    creating my frist angular component

---
+++ weather-api/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"
+
+@Component({
+
+    selector: 'meu-primeiro-component',
+    template:`<p>This is my first component in ANGULAR!</p>`
+    
+})
+export class MeuPrimeiroComponent {}
